a96d472ff098c3fdc577495150aca980036d9e61
li <1308341987@qq.com>
2018-01-20 19:57:58 +0800

    创建项目

---
+++ e3-manager/e3-manager-dao/src/main/java/cn/zcib/e3shop/mapper/TbUserMapper.java
@@ -0,0 +1,30 @@
+package cn.zcib.e3shop.mapper;
+
+import cn.zcib.e3shop.pojo.TbUser;
+import cn.zcib.e3shop.pojo.TbUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TbUserMapper {
+    int countByExample(TbUserExample example);
+
+    int deleteByExample(TbUserExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbUser record);
+
+    int insertSelective(TbUser record);
+
+    List<TbUser> selectByExample(TbUserExample example);
+
+    TbUser selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbUser record, @Param("example") TbUserExample example);
+
+    int updateByExample(@Param("record") TbUser record, @Param("example") TbUserExample example);
+
+    int updateByPrimaryKeySelective(TbUser record);
+
+    int updateByPrimaryKey(TbUser record);
+}
